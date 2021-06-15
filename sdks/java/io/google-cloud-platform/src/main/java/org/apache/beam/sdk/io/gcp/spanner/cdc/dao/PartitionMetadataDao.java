2b198fb8c506005624f593ebe5017ad0261346bd
Thiago Nunes <thiagotnunes@google.com>
2021-06-15 17:01:45 +1000

    refactor: organize partition metadata dao

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/cdc/dao/PartitionMetadataDao.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/cdc/dao/PartitionMetadataDao.java
@@ -1,14 +1,11 @@
 package org.apache.beam.sdk.io.gcp.spanner.cdc.dao;
 
-import com.google.cloud.Timestamp;
 import com.google.cloud.spanner.DatabaseClient;
 import com.google.cloud.spanner.Mutation;
 import com.google.cloud.spanner.TransactionContext;
-import com.google.common.collect.ImmutableList;
-import java.util.Collections;
+import com.google.cloud.spanner.Value;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import org.apache.beam.sdk.io.gcp.spanner.cdc.model.PartitionMetadata;
 
 // TODO: Integration test
@@ -28,23 +25,13 @@ public class PartitionMetadataDao {
 
   private final DatabaseClient databaseClient;
 
+  // TODO: We should receive a table name in the constructor
   public PartitionMetadataDao (DatabaseClient databaseClient) {
     this.databaseClient = databaseClient;
   }
 
-  public Timestamp insert(String table, PartitionMetadata partitionMetadata) {
-    final Mutation mutation = insertMutationFrom(table, partitionMetadata);
-    return databaseClient.write(ImmutableList.of(mutation));
-  }
-
   // TODO: Implement
-  public long countPartitionsInStates(
-      List<String> partitionTokens,
-      List<PartitionMetadata.State> states) {
-    throw new UnsupportedOperationException("Unimplemented");
-  }
-
-  // TODO: Implement
+  // This should be the query as in https://docs.google.com/document/d/1SxD3L46vlAtAdC2wEFjhYuGuddE3AvkB1HFRnQ3yrzs/edit?resourcekey=0-k6ifYSXSlw0UQLRtVwnLlQ#bookmark=id.q712q48z4rl
   public long countChildPartitionsInStates(
       String partitionToken,
       List<PartitionMetadata.State> states) {
@@ -52,46 +39,41 @@ public class PartitionMetadataDao {
   }
 
   // TODO: Implement
-  public void updateState(String partitionToken, PartitionMetadata.State state) {
+  // This should be the query as in https://docs.google.com/document/d/1SxD3L46vlAtAdC2wEFjhYuGuddE3AvkB1HFRnQ3yrzs/edit?resourcekey=0-k6ifYSXSlw0UQLRtVwnLlQ#
+  public long countExistingParents(String partitionToken) {
     throw new UnsupportedOperationException("Unimplemented");
   }
 
+  // FIXME: This should be unified with the method below (without the stable name as a parameter)
+  public void insert(String table, PartitionMetadata partitionMetadata) {
+    runInTransaction(transaction -> transaction.insert(partitionMetadata));
+  }
+
+  // FIXME: This should be unified with the method above
   public void insert(PartitionMetadata row) {
-    this.insert(Collections.singletonList(row));
+    runInTransaction(transaction -> transaction.insert(row));
   }
 
-  public void insert(List<PartitionMetadata> rows) {
-    // FIXME: add table name here
-    final List<Mutation> mutations = rows
-        .stream()
-        .map(row -> PartitionMetadataDao.insertMutationFrom(null, row))
-        .collect(Collectors.toList());
-
-    databaseClient.write(mutations);
+  public long updateState(
+      String partitionToken,
+      PartitionMetadata.State state) {
+    return runInTransaction(transaction -> transaction.updateState(partitionToken, state));
   }
 
-  // TODO: Remove if not necessary
+  public long delete(String partitionToken) {
+    return runInTransaction(transaction -> transaction.delete(partitionToken));
+  }
+
   public <T> T runInTransaction(Function<InTransactionContext, T> callable) {
-    // FIXME: table name should be given in the constructor
     return databaseClient
         .readWriteTransaction()
         .run(transaction -> {
+          // FIXME: table name should be given in the constructor instead of null
           final InTransactionContext dao = new InTransactionContext(null, transaction);
           return callable.apply(dao);
         });
   }
 
-  // TODO: Implement
-  public long countExistingParents(String partitionToken) {
-    throw new UnsupportedOperationException("Unimplemented");
-  }
-
-  // TODO: Implement
-  public void delete(String partitionToken) {
-    throw new UnsupportedOperationException("Unimplemented");
-  }
-
-  // TODO: Remove if not necessary
   public static class InTransactionContext {
 
     private final String tableName;
@@ -102,31 +84,20 @@ public class PartitionMetadataDao {
       this.transaction = transaction;
     }
 
-    public void insert(List<PartitionMetadata> rows) {
-      final List<Mutation> mutations = rows
-          .stream()
-          .map(row -> PartitionMetadataDao.insertMutationFrom(tableName, row))
-          .collect(Collectors.toList());
-
-      transaction.buffer(mutations);
+    // TODO: Check implementation
+    public Void insert(PartitionMetadata row) {
+      transaction.buffer(insertMutationFrom(tableName, row));
+      return null;
     }
 
-    public void insert(PartitionMetadata row) {
-      this.insert(Collections.singletonList(row));
+    // TODO: Implement
+    public long updateState(String partitionToken, PartitionMetadata.State state) {
+      throw new UnsupportedOperationException("Unimplemented");
     }
 
-    public void updateState(String partitionToken, PartitionMetadata.State state) {
-      final Mutation mutation = Mutation
-          .newUpdateBuilder(tableName)
-          .set(COLUMN_PARTITION_TOKEN)
-          .to(partitionToken)
-          .set(COLUMN_STATE)
-          .to(state.toString())
-          // FIXME: This should be the pending commit timestamp
-          // .set(COLUMN_UPDATED_AT)
-          // .to(null)
-          .build();
-      transaction.buffer(mutation);
+    // TODO: Implement
+    public long delete(String partitionToken) {
+      throw new UnsupportedOperationException("Unimplemented");
     }
 
     // TODO: Implement
@@ -136,36 +107,33 @@ public class PartitionMetadataDao {
     ) {
       throw new UnsupportedOperationException("Unimplemented");
     }
-  }
 
-  // TODO: Make instance based if InTransactionContext is not used
-  private static Mutation insertMutationFrom(
-      String tableName,
-      PartitionMetadata partitionMetadata) {
-    return Mutation.newInsertBuilder(tableName)
-        .set(COLUMN_PARTITION_TOKEN)
-        .to(partitionMetadata.getPartitionToken())
-        // FIXME: This should be a list of parents
-        .set(COLUMN_PARENT_TOKEN)
-        .toStringArray(partitionMetadata.getParentTokens())
-        .set(COLUMN_START_TIMESTAMP)
-        .to(partitionMetadata.getStartTimestamp())
-        .set(COLUMN_INCLUSIVE_START)
-        .to(partitionMetadata.isInclusiveStart())
-        .set(COLUMN_END_TIMESTAMP)
-        .to(partitionMetadata.getEndTimestamp())
-        .set(COLUMN_INCLUSIVE_END)
-        .to(partitionMetadata.isInclusiveEnd())
-        .set(COLUMN_HEARTBEAT_SECONDS)
-        .to(partitionMetadata.getHeartbeatSeconds())
-        .set(COLUMN_STATE)
-        .to(partitionMetadata.getState().toString())
-        // FIXME: This should be the pending commit timestamp
-        .set(COLUMN_CREATED_AT)
-        .to(partitionMetadata.getCreatedAt())
-        // FIXME: This should be the pending commit timestamp
-        .set(COLUMN_UPDATED_AT)
-        .to(partitionMetadata.getUpdatedAt())
-        .build();
+    private Mutation insertMutationFrom(
+        String tableName,
+        PartitionMetadata partitionMetadata) {
+      return Mutation.newInsertBuilder(tableName)
+          .set(COLUMN_PARTITION_TOKEN)
+          .to(partitionMetadata.getPartitionToken())
+          // FIXME: This should be a list of parents
+          .set(COLUMN_PARENT_TOKEN)
+          .toStringArray(partitionMetadata.getParentTokens())
+          .set(COLUMN_START_TIMESTAMP)
+          .to(partitionMetadata.getStartTimestamp())
+          .set(COLUMN_INCLUSIVE_START)
+          .to(partitionMetadata.isInclusiveStart())
+          .set(COLUMN_END_TIMESTAMP)
+          .to(partitionMetadata.getEndTimestamp())
+          .set(COLUMN_INCLUSIVE_END)
+          .to(partitionMetadata.isInclusiveEnd())
+          .set(COLUMN_HEARTBEAT_SECONDS)
+          .to(partitionMetadata.getHeartbeatSeconds())
+          .set(COLUMN_STATE)
+          .to(partitionMetadata.getState().toString())
+          .set(COLUMN_CREATED_AT)
+          .to(Value.COMMIT_TIMESTAMP)
+          .set(COLUMN_UPDATED_AT)
+          .to(Value.COMMIT_TIMESTAMP)
+          .build();
+    }
   }
 }
